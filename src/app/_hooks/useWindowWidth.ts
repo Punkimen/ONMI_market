f93f881ba0a8c67132660f2b3acdabe7818a2a2d
Max <punkimen666@gmail.com>
2023-09-20 01:18:17 +0300

    adaptive

--- src/app/_hooks/useWindowWidth.ts
+++ src/app/_hooks/useWindowWidth.ts
@@ -2,16 +2,16 @@ import {useEffect, useState} from "react";
 
 
 export const useWindowWidth = () => {
-  const [windowWidth, setWindowWidth] = useState(0)
+  const [windowWidth, setWindowWidth] = useState(0);
 
   useEffect(() => {
     const windowResize = () => {
-      setWindowWidth(window.innerWidth)
-    }
-    window.addEventListener('resize', windowResize)
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', windowResize);
     windowResize();
 
-    return () => window.removeEventListener('resize', windowResize)
-  }, [])
+    return () => window.removeEventListener('resize', windowResize);
+  }, []);
   return windowWidth;
-}
+};
