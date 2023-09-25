886e8538cea4801cb6b13e5461f04f8253b4158e
Max <punkimen666@gmail.com>
2023-09-26 00:44:28 +0300

    wallet

--- src/app/_components/partials/WalletBlock/WalletBlock.tsx
+++ src/app/_components/partials/WalletBlock/WalletBlock.tsx
@@ -2,11 +2,26 @@ import React, {FC} from 'react';
 import s from './WalletBlock.module.scss';
 import cn from 'classnames';
 import {IBaseComponents} from "@/app/_types/base.types";
-interface IWalletBlock extends IBaseComponents {}
-export const WalletBlock:FC<IWalletBlock> = () => {
+import {BtnBig} from "@/app/_components/partials/Buttons/BtnBig/BtnBig";
+
+interface IWalletBlock extends IBaseComponents {
+  value: string,
+  course: string,
+  isSoon?: boolean,
+}
+
+export const WalletBlock: FC<IWalletBlock> = ({course, isSoon, value, className, hide}) => {
   return (
-    <div className={s.wallet}>
-      <div className={s.value}></div>
+    <div className={cn(s.wallet, isSoon && s.wallet_soon, className)}>
+      <div className={s.content}>
+        <div className={s.value}>{value}</div>
+        <div className={s.course}>{course}</div>
+      </div>
+      {!isSoon &&
+          <BtnBig className={cn(s.btn_top, s.btn)}>
+              + Top Up
+          </BtnBig>
+      }
     </div>
   );
 };
